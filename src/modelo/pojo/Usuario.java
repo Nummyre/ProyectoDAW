9d2b24e8368731505980d834d9440c352c814819
Cintia <cintiia.349@gmail.com>
2020-03-21 22:37:46 +0100

    Añadiendo email al pojo de usuario

--- src/modelo/pojo/Usuario.java
+++ src/modelo/pojo/Usuario.java
@@ -7,69 +7,65 @@ public class Usuario {
 	private String user;
 	private String password;
 	private String foto;
+	private String email;
 	private String fechaAlta;
 	
-	public Usuario(Integer id, String nombre, String user, String password, String foto, String fechaAlta) {
+	public Usuario(Integer id, String nombre, String user, String password, String foto, String email,
+			String fechaAlta) {
 		this.id = id;
 		this.nombre = nombre;
 		this.user = user;
 		this.password = password;
 		this.foto = foto;
+		this.email = email;
 		this.fechaAlta = fechaAlta;
 	}
 	
 	public Usuario() {
 		
 	}
-
+	
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getNombre() {
 		return nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 	public String getUser() {
 		return user;
 	}
-
 	public void setUser(String user) {
 		this.user = user;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 	public String getFoto() {
 		return foto;
 	}
-
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
-
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	public String getFechaAlta() {
 		return fechaAlta;
 	}
-
 	public void setFechaAlta(String fechaAlta) {
 		this.fechaAlta = fechaAlta;
 	}
-	
-	
 	
 }
