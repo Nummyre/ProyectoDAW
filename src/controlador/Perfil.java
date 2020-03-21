48473f1def85a550d3bb085204d0ec18f29877a8
Cintia <cintiia.349@gmail.com>
2020-03-21 18:25:02 +0100

    añadiendo sesiones, usuario al servlet Perfil

--- src/controlador/Perfil.java
+++ src/controlador/Perfil.java
@@ -1,41 +1,47 @@
 package controlador;
 
 import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class Perfil
- */
+import modelo.ejb.SesionesEJB;
+import modelo.ejb.UsuariosEJB;
+import modelo.pojo.Usuario;
+
+
 @WebServlet("/Perfil")
 public class Perfil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Perfil() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	@EJB
+	UsuariosEJB usuariosEJB;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	@EJB
+	SesionesEJB sesionesEJB;
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+		HttpSession session = request.getSession(false);
+
+		Usuario usuario = sesionesEJB.usuarioLogeado(session);
+		
+
+		request.setAttribute("usuario", usuario);
+		RequestDispatcher rs = getServletContext().getRequestDispatcher("/vista/Perfil.jsp");
+		rs.forward(request, response);
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	
 	}
 
 }
