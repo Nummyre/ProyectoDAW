702f1f9969a9a7923a3ed1c10302b64fcc5d2e1d
Cintia <cintiia.349@gmail.com>
2020-03-30 18:50:56 +0200

    añadido admin

--- src/modelo/pojo/Usuario.java
+++ src/modelo/pojo/Usuario.java
@@ -9,9 +9,11 @@ public class Usuario {
 	private String foto;
 	private String email;
 	private String fechaAlta;
+	private Integer administrador;
 	
-	public Usuario(Integer id, String nombre, String user, String password, String foto, String email,
-			String fechaAlta) {
+	public Usuario(Integer id, String nombre, String user, String password, String foto, String email, String fechaAlta,
+			Integer administrador) {
+		super();
 		this.id = id;
 		this.nombre = nombre;
 		this.user = user;
@@ -19,53 +21,79 @@ public class Usuario {
 		this.foto = foto;
 		this.email = email;
 		this.fechaAlta = fechaAlta;
+		this.administrador = administrador;
 	}
 	
 	public Usuario() {
 		
 	}
-	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getUser() {
 		return user;
 	}
+
 	public void setUser(String user) {
 		this.user = user;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getFoto() {
 		return foto;
 	}
+
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getFechaAlta() {
 		return fechaAlta;
 	}
+
 	public void setFechaAlta(String fechaAlta) {
 		this.fechaAlta = fechaAlta;
 	}
+
+	public Integer getAdministrador() {
+		return administrador;
+	}
+
+	public void setAdministrador(Integer administrador) {
+		this.administrador = administrador;
+	}
+	
+	
+	
+	
 	
 }
