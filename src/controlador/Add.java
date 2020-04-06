879f889aff3338f5bab311497424dde61a97e2ec
Cintia <cintiia.349@gmail.com>
2020-04-06 17:27:20 +0200

    añadiendo el usuario al servlet

--- src/controlador/Add.java
+++ src/controlador/Add.java
@@ -1,20 +1,40 @@
 package controlador;
 
 import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import modelo.ejb.SesionesEJB;
+import modelo.ejb.UsuariosEJB;
+import modelo.pojo.Usuario;
 
 @WebServlet("/Add")
 public class Add extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	@EJB
+	UsuariosEJB usuariosEJB;
 
+	@EJB
+	SesionesEJB sesionesEJB;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		HttpSession session = request.getSession(false);
+
+		Usuario usuario = sesionesEJB.usuarioLogeado(session);
+		
+
+		request.setAttribute("usuario", usuario);
+		RequestDispatcher rs = getServletContext().getRequestDispatcher("/vista/Add.jsp");
+		rs.forward(request, response);
 	}
 
 
