29e9c8487e37eac1baad9a637a1500be51f1516f
Cintia <cintiia.349@gmail.com>
2020-04-07 18:33:00 +0200

    haciendo el post

--- src/controlador/Add.java
+++ src/controlador/Add.java
@@ -1,16 +1,19 @@
 package controlador;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.ejb.EJB;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 
 import modelo.ejb.JuegoEJB;
 import modelo.ejb.SesionesEJB;
@@ -21,19 +24,22 @@ import modelo.pojo.Plataforma;
 import modelo.pojo.Usuario;
 
 @WebServlet("/Add")
+@MultipartConfig(maxFileSize = 1024 * 1024 * 5)
 public class Add extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private static final String UPLOAD_DIRECTORY = "Imagenes";
+
 	@EJB
 	UsuariosEJB usuariosEJB;
-	
+
 	@EJB
 	JuegoEJB juegoEJB;
 
 	@EJB
 	SesionesEJB sesionesEJB;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		HttpSession session = request.getSession(false);
 
@@ -44,14 +50,62 @@ public class Add extends HttpServlet {
 		request.setAttribute("listaJuego", juego);
 		request.setAttribute("usuario", usuario);
 		request.setAttribute("plataforma", juegoP);
-		
+
 		RequestDispatcher rs = getServletContext().getRequestDispatcher("/vista/Add.jsp");
 		rs.forward(request, response);
 	}
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String titulo = request.getParameter("titulo");
+		String any = request.getParameter("anyo");
+		String gen = request.getParameter("gen");
+		String pla = request.getParameter("pla");
+		String desc = request.getParameter("desc");
+		String idUser = request.getParameter("id");
+		Integer idGuia = 0;
+		Integer idAnali = 0;
 
+		Integer anyo = Integer.parseInt(any);
+		Integer genero = Integer.parseInt(gen);
+		Integer plataforma = Integer.parseInt(pla);
+		Integer id = Integer.parseInt(idUser);
+
+		// Multipart RFC 7578
+
+		// Obtenemos una ruta en el servidor para guardar el archivo
+		String uploadPath = getServletContext().getRealPath("") + File.separator + UPLOAD_DIRECTORY;
+
+		// Si la ruta no existe la crearemos
+		File uploadDir = new File(uploadPath);
+		if (!uploadDir.exists()) {
+			uploadDir.mkdir();
+		}
+
+		// Lo utilizaremos para guardar el nombre del archivo
+		String fileName = null;
+
+		// Obtenemos el archivo y lo guardamos a disco
+		for (Part part : request.getParts()) {
+			fileName = getFileName(part);
+			part.write(uploadPath + File.separator + fileName);
+		}
+
+		juegoEJB.insertJuego(titulo, desc, anyo, genero, plataforma, id);
+		juegoEJB.insertJuegoFoto(fileName, id, idGuia, idAnali);
+
+		response.sendRedirect("Login");
+
+	}
+
+	// Obtiene el nombre del archivo, sino lo llamaremos desconocido.txt
+	private String getFileName(Part part) {
+		for (String content : part.getHeader("content-disposition").split(";")) {
+			if (content.trim().startsWith("filename"))
+				return content.substring(content.indexOf("=") + 2, content.length() - 1);
+		}
+		return "desconocido.txt";
 	}
 
 }
