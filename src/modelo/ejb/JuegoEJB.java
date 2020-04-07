a159daa11d6d707d8629a1a074dff38346fc149c
Cintia <cintiia.349@gmail.com>
2020-04-07 18:33:35 +0200

    añadiendo insert de juego y foto

--- src/modelo/ejb/JuegoEJB.java
+++ src/modelo/ejb/JuegoEJB.java
@@ -16,21 +16,36 @@ public class JuegoEJB {
 
 	public ArrayList<Juego> listaJuego() {
 		JuegosDAO juegoList = new JuegosDAO();
-		
+
 		return juegoList.listaJuegos();
 	}
-	
+
 	public ArrayList<Genero> genero() {
 		JuegosDAO juegoList = new JuegosDAO();
-		
+
 		return juegoList.genero();
 	}
-	
-	public ArrayList<Plataforma> plataforma(){
-		
+
+	public ArrayList<Plataforma> plataforma() {
+
 		JuegosDAO juegoList = new JuegosDAO();
-		
+
 		return juegoList.plataforma();
+	}
+
+	public void insertJuego(String titulo, String desc, Integer anyo, Integer idGen, Integer idPla, Integer idUser) {
+
+		JuegosDAO juegoList = new JuegosDAO();
+
+		juegoList.insertJuego(titulo, desc, anyo, idGen, idPla, idUser);
+		
+	}
+
+	public void insertJuegoFoto(String foto, Integer idJuego, Integer idGuia, Integer idAnali) {
+
+		JuegosDAO juegoList = new JuegosDAO();
+
+		juegoList.insertJuegoFoto(foto, idJuego, idGuia, idAnali);
 	}
 
 }
