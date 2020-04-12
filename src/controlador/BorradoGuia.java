dc7cdf1e9d6692f8a92a51cb30867623c6ae3c48
Cintia <cintiia.349@gmail.com>
2020-04-12 20:00:53 +0200

    creacion del servlet borrar una guia (hace la funcion de borrar una guia)

---
+++ src/controlador/BorradoGuia.java
@@ -0,0 +1,49 @@
+package controlador;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import modelo.ejb.JuegoEJB;
+import modelo.ejb.SesionesEJB;
+import modelo.ejb.UsuariosEJB;
+import modelo.pojo.Usuario;
+
+
+@WebServlet("/BorradoGuia")
+public class BorradoGuia extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@EJB
+	JuegoEJB juegoEJB;
+
+	@EJB
+	UsuariosEJB usuariosEJB;
+
+	@EJB
+	SesionesEJB sesionesEJB;
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession(false);
+
+		Usuario usuario = sesionesEJB.usuarioLogeado(session);
+
+		String id = request.getParameter("id");
+
+		Integer idJ = Integer.parseInt(id);
+
+		juegoEJB.deleteGuia(idJ);
+		
+
+		response.sendRedirect("BorrarListaGuia?id="+usuario.getId());
+	}
+
+
+}
