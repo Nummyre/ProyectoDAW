a24a4a273e2a4ccfd788a1061a5d37f07687170d
Cintia <cintiia.349@gmail.com>
2020-04-27 20:30:29 +0200

    comentario pojo

--- src/modelo/pojo/Comentario.java
+++ src/modelo/pojo/Comentario.java
@@ -3,53 +3,49 @@ package modelo.pojo;
 public class Comentario {
 
 	private Integer id;
-	private String comentario;
 	private Integer idUsuario;
 	private Integer idJuego;
+	private String comentario;
 	
-	public Comentario(Integer id, String comentario, Integer idUsuario, Integer idJuego) {
-		this.id = id;
-		this.comentario = comentario;
-		this.idUsuario = idUsuario;
-		this.idJuego = idJuego;
-	}
-	
-	public Comentario() {
+	public Comentario () {
 		
 	}
-
+	
+	public Comentario(Integer id, Integer idUsuario, Integer idJuego, String comentario) {
+		super();
+		this.id = id;
+		this.idUsuario = idUsuario;
+		this.idJuego = idJuego;
+		this.comentario = comentario;
+	}
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
-	public String getComentario() {
-		return comentario;
-	}
-
-	public void setComentario(String comentario) {
-		this.comentario = comentario;
-	}
-
 	public Integer getIdUsuario() {
 		return idUsuario;
 	}
-
 	public void setIdUsuario(Integer idUsuario) {
 		this.idUsuario = idUsuario;
 	}
-
 	public Integer getIdJuego() {
 		return idJuego;
 	}
-
 	public void setIdJuego(Integer idJuego) {
 		this.idJuego = idJuego;
 	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
 	
+	
+	
+
 	
 	
 }
