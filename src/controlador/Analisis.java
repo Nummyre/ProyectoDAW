f67bf1c3688f7019cebcdcd6e7436f9b12de7664
Cintia <cintiia.349@gmail.com>
2020-04-29 19:51:38 +0200

    servlet para msotrar lista de analisis

---
+++ src/controlador/Analisis.java
@@ -0,0 +1,53 @@
+package controlador;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import modelo.ejb.JuegoEJB;
+import modelo.ejb.SesionesEJB;
+import modelo.ejb.UsuariosEJB;
+import modelo.pojo.Usuario;
+
+
+@WebServlet("/Analisis")
+public class Analisis extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@EJB
+	UsuariosEJB usuariosEJB;
+
+	@EJB
+	SesionesEJB sesionesEJB;
+	
+	@EJB
+	JuegoEJB juegoEJB;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		HttpSession session = request.getSession(false);
+
+		Usuario usuario = sesionesEJB.usuarioLogeado(session);
+		
+		 ArrayList<modelo.pojo.Analisis> analisis = juegoEJB.listaAnalisis();
+
+		 
+		request.setAttribute("usuario", usuario);
+		request.setAttribute("analisis", analisis);
+		
+		RequestDispatcher rs = getServletContext().getRequestDispatcher("/vista/Analisis.jsp");
+		rs.forward(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
