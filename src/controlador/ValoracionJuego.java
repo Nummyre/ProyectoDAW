f06c0260b9edce59455f7c4269dc4be919861aac
cintia <cintiia.349@gmail.com>
2020-04-30 17:19:59 +0200

    servlet para puntuar el juego

---
+++ src/controlador/ValoracionJuego.java
@@ -0,0 +1,54 @@
+package controlador;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import modelo.ejb.JuegoEJB;
+import modelo.ejb.SesionesEJB;
+import modelo.ejb.UsuariosEJB;
+import modelo.pojo.Juego;
+import modelo.pojo.Usuario;
+
+@WebServlet("/ValoracionJuego")
+public class ValoracionJuego extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	@EJB
+	UsuariosEJB usuariosEJB;
+
+	@EJB
+	SesionesEJB sesionesEJB;
+	
+	@EJB
+	JuegoEJB juegoEJB;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		HttpSession session = request.getSession(false);
+
+		Usuario usuario = sesionesEJB.usuarioLogeado(session);
+		
+		String es = request.getParameter("estrellas");
+		String idJ = request.getParameter("idJ");
+		
+		Integer id = Integer.parseInt(idJ);
+		Integer estrella = Integer.parseInt(es);
+		
+		juegoEJB.updateValoracion(estrella, id);
+		
+		response.sendRedirect("FichaJuego?id="+id);
+		
+	}
+
+	
+
+}
