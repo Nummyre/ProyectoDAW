a105a1b04a16d305bc2caf7bd84ac361dd2d2247
cintia <cintiia.349@gmail.com>
2020-04-30 18:22:27 +0200

    pojo puntuacion

---
+++ src/modelo/pojo/Puntuacion.java
@@ -0,0 +1,78 @@
+package modelo.pojo;
+
+public class Puntuacion {
+	
+	private Integer id;
+	private Integer puntuacion;
+	private Integer idJuego;
+	private Integer idUsuario;
+	
+	
+	
+	public Puntuacion() {
+		
+	}
+
+
+	
+	
+
+	public Puntuacion(Integer id, Integer puntuacion, Integer idJuego, Integer idUsuario) {
+		super();
+		this.id = id;
+		this.puntuacion = puntuacion;
+		this.idJuego = idJuego;
+		this.idUsuario = idUsuario;
+	}
+
+
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+
+	public Integer getPuntuacion() {
+		return puntuacion;
+	}
+
+
+
+	public void setPuntuacion(Integer puntuacion) {
+		this.puntuacion = puntuacion;
+	}
+
+
+
+	public Integer getIdJuego() {
+		return idJuego;
+	}
+
+
+
+	public void setIdJuego(Integer idJuego) {
+		this.idJuego = idJuego;
+	}
+
+
+
+	public Integer getIdUsuario() {
+		return idUsuario;
+	}
+
+
+
+	public void setIdUsuario(Integer idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	
+}
