ed92c9dec1204ff8c0f82e8dd2635f05680cec51
cintia <cintiia.349@gmail.com>
2020-05-02 18:38:09 +0200

    top10 java servlet

---
+++ src/controlador/Top10.java
@@ -0,0 +1,58 @@
+package controlador;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import modelo.ejb.JuegoEJB;
+import modelo.ejb.SesionesEJB;
+import modelo.ejb.UsuariosEJB;
+import modelo.pojo.Foto;
+import modelo.pojo.Usuario;
+
+@WebServlet("/Top10")
+public class Top10 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	   
+		@EJB
+		UsuariosEJB usuariosEJB;
+
+		@EJB
+		SesionesEJB sesionesEJB;
+		
+		@EJB
+		JuegoEJB juegoEJB;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		HttpSession session = request.getSession(false);
+
+		Usuario usuario = sesionesEJB.usuarioLogeado(session);
+		
+		ArrayList<modelo.pojo.Top10> top = juegoEJB.listaTop10();
+		
+		 ArrayList<Foto> foto = juegoEJB.listaFotosJuegos();
+		 
+		 request.setAttribute("foto", foto);
+			request.setAttribute("usuario", usuario);
+			request.setAttribute("top", top);
+			
+			RequestDispatcher rs = getServletContext().getRequestDispatcher("/vista/Top10.jsp");
+			rs.forward(request, response);
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+}
